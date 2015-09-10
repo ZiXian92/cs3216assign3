c04e2c0614f17c820af13442c88b69a286aa20b0
zixian92 <zixian1992@hotmail.com>
2015-09-10 14:52:04 +0800

    Adds slider to home page

--- dev-deploy.sh
+++ dev-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pip3 install -r requirements.txt
-python3 run.py &
+python3 src/run.py &
--- src/app/static/app.js
+++ src/app/static/app.js
@@ -1,5 +1,18 @@
-var app = angular.module('tldr', ['titleBar', 'sideNav']);
+var app = angular.module('tldr', ['titleBar', 'sideNav', 'ngRoute']);
 
 app.controller('mainController', function($scope){
 	
-});
+}).controller('homeController', function($scope){
+	$(document).ready(function(){
+		$('.slider').slider({
+			height: 300
+		});
+	});
+}).config(['$routeProvider', function($routeProvider){
+	$routeProvider.when('/', {
+		templateUrl: '/static/partials/home.html',
+		controller: 'homeController'
+	}).otherwise({
+		redirectTo: '/'
+	});
+}]);
