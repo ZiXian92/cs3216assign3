3513823078c3c957423591ecc15ac2babee7eb23
CNA-Bld <cna.bld@gmail.com>
2015-09-10 21:47:19 +0800

    add memcache to post api

---
+++ src/app/utils.py
@@ -0,0 +1,15 @@
+from app import mc, parsers
+
+
+def post_key(source_id, post_id):
+    return 'POST:{}:{}'.format(source_id, post_id)
+
+
+def get_cached_post(source_id, post_id):
+    cached_data = mc.get(post_key(source_id, post_id))
+    if cached_data:
+        return cached_data
+    parser = parsers.parsers[source_id]
+    data = parser.parse_post(post_id)
+    mc.set(post_key(source_id, post_id), data, time=3600)
+    return data
--- src/app/views.py
+++ src/app/views.py
@@ -1,5 +1,4 @@
-from app import app, api
-from app.parsers import parsers
+from app import app, api, utils
 
 from flask import render_template
 
@@ -8,12 +7,12 @@ from flask_restful import Resource
 
 class Post(Resource):
     def get(self, source_id, post_id):
-        parser = parsers[source_id]
-        return parser.parse_post(post_id)
+        return utils.get_cached_post(source_id, post_id)
 
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
 
 api.add_resource(Post, '/post/<string:source_id>/<string:post_id>')
