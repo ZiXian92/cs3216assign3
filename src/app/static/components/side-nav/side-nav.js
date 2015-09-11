382af9a1bf3bff8438c0402c3bcf9cc7757c66f0
zixian92 <zixian1992@hotmail.com>
2015-09-11 23:01:34 +0800

    Adds home page banner images

--- src/app/static/components/side-nav/side-nav.js
+++ src/app/static/components/side-nav/side-nav.js
@@ -8,14 +8,12 @@ sideNav.directive('sideNav', ['$mdSidenav', function($mdSidenav){
 			page: '='
 		},
 		controller: function($scope, $mdSidenav){
-			console.log($scope.page);
 			$scope.$watch('page', function(newVal, oldVal, scope){
 				console.log(newVal);
 			});
 
 			// Event Handlers
 			$scope.onMenuSelect = function(menu){
-				console.log('Changing page');
 				$mdSidenav('side-bar').close();
 				$scope.$emit('pageChange', menu);
 			};
