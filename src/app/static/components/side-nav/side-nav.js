7a90881f276e420e575ec70c559f7d12fdc09f75
CNA-Bld <cna.bld@gmail.com>
2015-09-17 00:35:09 +0800

    improve side nav

--- src/app/static/components/side-nav/side-nav.js
+++ src/app/static/components/side-nav/side-nav.js
@@ -1,6 +1,6 @@
 var sideNav = angular.module('sideNav', ['ngMaterial', 'ngMdIcons']);
 
-sideNav.directive('sideNav', ['sidenavService', function(sidenavService){
+sideNav.directive('sideNav', ['$location', 'sidenavService', function($location, sidenavService){
 	return {
 		restrict: 'A',
 		templateUrl: '/static/components/side-nav/side-nav.html',
@@ -13,10 +13,48 @@ sideNav.directive('sideNav', ['sidenavService', function(sidenavService){
 			});
 
 			// Event Handlers
-			$scope.onMenuSelect = function(menu){
+			$scope.onMenuSelect = function(menu, view){
 				sidenavService.closeSidenav();
 				$scope.$emit('pageChange', menu);
+				$location.path(view);
 			};
+
+			$scope.sideMenuItems = [{
+				'title': 'Latest Articles',
+				'iid': 'latest',
+				'view': '/feed',
+				'icon': 'new_releases'
+			}, {
+				'title': 'Popular Articles',
+				'iid': 'popular',
+				'view': '/feed',
+				'icon': 'trending_up'
+			}, {
+				'title': 'Money',
+				'iid': 'money',
+				'view': '/feed/2',
+				'icon': 'attach_money'
+			}, {
+				'title': 'Technology',
+				'iid': 'technology',
+				'view': '/feed/3',
+				'icon': 'devices'
+			}, {
+				'title': 'Work',
+				'iid': 'work',
+				'view': '/feed/4',
+				'icon': 'work'
+			}, {
+				'title': 'Lifestyle',
+				'iid': 'lifestyle',
+				'view': '/feed/5',
+				'icon': 'mood'
+			}, {
+				'title': 'About tl;dr',
+				'iid': 'about',
+				'view': '/about',
+				'icon': 'info'
+			}];
 		},
 		link: function(scope, element, attrs){
 
