a6548d02291482c7bb9099e075ddcd165ad63089
zixian92 <zixian1992@hotmail.com>
2015-09-20 20:39:38 +0800

    Restores link to home page on app logo

--- src/app/static/components/title-bar/title-bar.js
+++ src/app/static/components/title-bar/title-bar.js
@@ -5,9 +5,11 @@ titleBar.directive('titleBar', ['sidenavService', function(sidenavService){
 		restrict: 'A',
 		templateUrl: '/static/components/title-bar/title-bar.html',
 		scope: true,
-		controller: function($scope){
-			$scope.showSideBar = function(){
-				sidenavService.openSidenav();
+		controller: function($scope, $location){
+			$scope.showSideBar = sidenavService.openSidenav;
+
+			$scope.onClickLogo = function(){
+				$location.path('/');
 			};
 		}, link: function(scope, element, attrs){
 			
